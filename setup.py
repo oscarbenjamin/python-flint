7d19ca3c3edc9f45b85299a7e207092e70f42b30
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2025-09-10 15:36:07 +0100

    simplify setup.py

--- setup.py
+++ setup.py
@@ -1,47 +1,24 @@
-import sys
-import os
-from subprocess import check_call
-
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-
-from setuptools import setup
-from setuptools.extension import Extension
-
-
-define_macros = [("Py_LIMITED_API", 0x03070000)]
-compiler_directives = {
-    'language_level': 3,
-    'binding': True,
-}
-
-
-packages = [
-    'flint',
-    'flint.flint_base',
-    'flint.types',
-]
-
-
-ext_files = [
-    ("flint.flint_base.flint_base", ["src/flint/flint_base/flint_base.pyx"]),
-    ("flint.types.fmpz", ["src/flint/types/fmpz.pyx"]),
-]
-
-ext_options = {
-    "define_macros" : define_macros,
-    "py_limited_api": True,
-}
-
-ext_modules = []
-for mod_name, src_files in ext_files:
-    ext = Extension(mod_name, src_files, **ext_options)
-    ext_modules.append(ext)
-
 setup(
-    cmdclass={'build_ext': build_ext},
-    ext_modules=cythonize(ext_modules, compiler_directives=compiler_directives),
-    packages=packages,
+    ext_modules=cythonize([
+        Extension(
+            name="flint.flint_base.flint_base",
+            sources=["src/flint/flint_base/flint_base.pyx"],
+            define_macros=[
+                ("Py_LIMITED_API", 0x03070000),
+            ],
+            py_limited_api=True
+        ),
+        Extension(
+            name="flint.types.fmpz",
+            sources=["src/flint/types/fmpz.pyx"],
+            define_macros=[
+                ("Py_LIMITED_API", 0x03070000),
+            ],
+            py_limited_api=True
+        ),
+    ]),
     package_dir={'': 'src'},
 )
