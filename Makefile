a11eb72c58267be0ee198e757f621faa92260ab5
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2025-09-10 15:42:45 +0100

    Add --limited-api to setup.py

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ test:
 	git clean -fdx flint
 	python setup.py build_ext --inplace
 	PYTHONPATH=src python bug.py
+test_limited:
+	git clean -fdx flint
+	python setup.py --limited-api build_ext --inplace
+	PYTHONPATH=src python bug.py
--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
+import sys
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+
+if '--limited-api' in sys.argv:
+    sys.argv.remove('--limited-api')
+    define_macros = [("Py_LIMITED_API", 0x03070000)]
+    py_limited_api = True
+else:
+    define_macros = []
+    py_limited_api = False
 
 setup(
     ext_modules=cythonize([
         Extension(
             name="flint.flint_base.flint_base",
             sources=["flint/flint_base/flint_base.pyx"],
-            define_macros=[
-                ("Py_LIMITED_API", 0x03070000),
-            ],
-            py_limited_api=True
+            define_macros=define_macros,
+            py_limited_api=py_limited_api
         ),
         Extension(
             name="flint.types.fmpz",
             sources=["flint/types/fmpz.pyx"],
-            define_macros=[
-                ("Py_LIMITED_API", 0x03070000),
-            ],
-            py_limited_api=True
+            define_macros=define_macros,
+            py_limited_api=py_limited_api
         ),
     ]),
 )
